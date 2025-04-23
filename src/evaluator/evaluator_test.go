dab7d75f250987f4ea2661ded1fc9b43ebe407df
Pablo Perez <pablo.perez.rodriguez@udc.es>
2025-04-23 18:05:09 -0500

    evaluating basic expressions

---
+++ src/evaluator/evaluator_test.go
@@ -0,0 +1,112 @@
+package evaluator
+
+import (
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(assert, evaluated, tt.expected)
+	}
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(assert, evaluated, tt.expected)
+	}
+}
+
+func TestBangOperator(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(assert, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(assert *assert.Assertions, obj object.Object, expected int64) {
+	result, ok := obj.(*object.Integer)
+	assert.True(ok, "object is not Integer, got=%T (%+v)", obj, obj)
+	assert.Equal(expected, result.Value, "object has wrong value")
+}
+
+func testBooleanObject(assert *assert.Assertions, obj object.Object, expected bool) {
+	result, ok := obj.(*object.Boolean)
+	assert.True(ok, "object is not Boolean, got=%T (%+v)", obj, obj)
+	assert.Equal(expected, result.Value, "object has wrong value")
+}
