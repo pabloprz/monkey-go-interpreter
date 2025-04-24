54236543fff576c4213105028670330192df9e98
Pablo Perez <pablo.perez.rodriguez@udc.es>
2025-04-24 18:37:24 -0500

    evaluating let statements and identifiers

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -147,6 +147,23 @@ if (10 > 1) {
 	}
 }
 
+func TestLetStatements(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(assert, testEval(tt.input), tt.expected)
+	}
+}
+
 func TestErrorHandling(t *testing.T) {
 	tests := []struct {
 		input           string
@@ -188,6 +205,10 @@ if (10 > 1) {
 `,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -204,8 +225,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 func testIntegerObject(assert *assert.Assertions, obj object.Object, expected int64) {
